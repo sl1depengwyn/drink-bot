da160aba0f5c89528bc97c2f659de97ee12d06b9
Max <max.pengwyn@gmail.com>
2022-06-12 21:59:17 +0300

    refactor tg module

--- src/Tg.hs
+++ src/Tg.hs
@@ -1,4 +1,4 @@
-module Tg where
+module Tg (run) where
 
 import qualified Bot
 import Control.Monad
@@ -44,7 +44,7 @@ longPolling h =
   forever $ do
     st <- get
     let offset = sOffset st
-    let logh = Bot.hLogger h
+        logh = Bot.hLogger h
     eitherUpdates <- liftIO $ getUpdates h offset
     case eitherUpdates of
       (Right updates) -> do
@@ -143,14 +143,18 @@ buildRequest host path query =
   setRequestHost host $
     setRequestQueryString query $
       setRequestPath path $
-        setRequestSecure True $ setRequestPort 443 $ setRequestResponseTimeout responseTimeoutNone defaultRequest
+        setRequestSecure True $
+          setRequestPort 443 $
+            setRequestResponseTimeout
+              responseTimeoutNone
+              defaultRequest
 
 sendRequest :: MonadIO m => Bot.Handle -> T.Text -> Query -> m (Response BC.ByteString)
 sendRequest h method query = do
   let host = (Bot.hUrl . Bot.cHost . Bot.hConfig) h
-  let apiKey = (Bot.cToken . Bot.hConfig) h
-  let path = mconcat ["/bot", apiKey, method]
-  let logh = Bot.hLogger h
+      apiKey = (Bot.cToken . Bot.hConfig) h
+      path = mconcat ["/bot", apiKey, method]
+      logh = Bot.hLogger h
   httpBS $ buildRequest host (T.encodeUtf8 path) query
 
 getUpdates :: MonadIO m => Bot.Handle -> Maybe Int -> m (Either String [Update])
@@ -158,18 +162,17 @@ getUpdates h offset = do
   let query = [("timeout", Just "25"), ("offset", BC.pack . show <$> offset)]
   response <- sendRequest h "/getUpdates" query
   let responseBody = getResponseBody response
-  let updates = A.eitherDecodeStrict responseBody
+      updates = A.eitherDecodeStrict responseBody
   pure $ uResult <$> updates
 
 sendMessage :: MonadIO m => Bot.Handle -> Int -> T.Text -> Query -> m ()
 sendMessage h usrId method query = do
   let chatId = ("chat_id", Just $ (BC.pack . show) usrId)
-  kb' <- liftIO $ getKeyboard h usrId
-  let finalQuery = chatId : query
+      finalQuery = chatId : query
   response <- sendRequest h method finalQuery
   let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
-  let respParser obj =
+      logh = Bot.hLogger h
+      respParser obj =
         case A.parseMaybe (\o -> (o A..: "result") >>= (A..: "message_id")) obj of
           (Just mId) -> liftIO $ Database.updateLastMessageId dbh usrId mId
           Nothing -> liftIO $ Logger.error logh "No message id found in response to send message"
@@ -180,13 +183,13 @@ sendMessage h usrId method query = do
 sendPhotoWithSumAndKb :: MonadIO m => Bot.Handle -> Int -> m ()
 sendPhotoWithSumAndKb h usrId = do
   let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
+      logh = Bot.hLogger h
   msgText' <- getTextWithAmount h usrId
   case msgText' of
     (Just msgText) -> do
       kb <- liftIO $ getKeyboard h usrId
       let fileId = Bot.cPicture . Bot.hConfig $ h
-      let query =
+          query =
             [ ("photo", Just (T.encodeUtf8 fileId)),
               ("caption", Just (T.encodeUtf8 msgText)),
               ("reply_markup", Just $ A.encodeStrict kb)
@@ -206,12 +209,12 @@ sendSticker botH fileId usrId = sendMessage botH usrId "/sendSticker" query
 
 sendStartMsg :: MonadIO m => Bot.Handle -> Int -> m ()
 sendStartMsg h usrId = do
-  let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
-  let msgText = Bot.cStartMessage . Bot.hConfig $ h
   kb <- liftIO $ getKeyboard h usrId
-  let fileId = Bot.cPicture . Bot.hConfig $ h
-  let query =
+  let dbh = Bot.hDatabase h
+      logh = Bot.hLogger h
+      msgText = Bot.cStartMessage . Bot.hConfig $ h
+      fileId = Bot.cPicture . Bot.hConfig $ h
+      query =
         [ ("photo", Just (T.encodeUtf8 fileId)),
           ("caption", Just (T.encodeUtf8 msgText)),
           ("reply_markup", Just $ A.encodeStrict kb)
@@ -227,17 +230,17 @@ sendFailMsg h = sendTextMessage h (Bot.cFailMessage . Bot.hConfig $ h)
 editMessage :: MonadIO m => Bot.Handle -> Int -> Int -> Query -> m ()
 editMessage h usrId mId query' = do
   let query = [("message_id", Just $ (BC.pack . show) mId), ("chat_id", Just $ (BC.pack . show) usrId)] ++ query'
-  let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
+      dbh = Bot.hDatabase h
+      logh = Bot.hLogger h
+      logMessage = mconcat ["edited message with id ", show mId, " with query ", show query]
   resp <- sendRequest h "/editMessageCaption" query
   liftIO $ Logger.debug logh (BC.unpack $ getResponseBody resp)
-  let logMessage = mconcat ["edited message with id ", show mId, " with query ", show query]
   liftIO $ Logger.info logh logMessage
 
 editLastMessage :: Bot.Handle -> Int -> IO ()
 editLastMessage h usrId = do
   let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
+      logh = Bot.hLogger h
   msgText' <- getTextWithAmount h usrId
   mIdToEdit <- Database.getLastMessageId dbh usrId
   case (msgText', mIdToEdit) of
@@ -253,10 +256,10 @@ editLastMessage h usrId = do
 editLastMessagePhoto :: Bot.Handle -> Int -> FilePath -> IO ()
 editLastMessagePhoto h usrId pathToPhoto = do
   let host = Bot.hUrl . Bot.cHost . Bot.hConfig $ h
-  let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
-  let apiKey = (Bot.cToken . Bot.hConfig) h
-  let path = mconcat ["/bot", apiKey, "/editMessageMedia"]
+      dbh = Bot.hDatabase h
+      logh = Bot.hLogger h
+      apiKey = (Bot.cToken . Bot.hConfig) h
+      path = mconcat ["/bot", apiKey, "/editMessageMedia"]
   kb <- liftIO $ getKeyboard h usrId
   mIdToEdit <- Database.getLastMessageId dbh usrId
   msgText' <- getTextWithAmount h usrId
@@ -284,7 +287,7 @@ editLastMessagePhoto h usrId pathToPhoto = do
 processMessage :: MonadIO m => Bot.Handle -> Message -> m ()
 processMessage h (UnsupportedMessage user) = do
   let logh = Bot.hLogger h
-  let logmsg = mconcat ["got unsupported message from ", show user]
+      logmsg = mconcat ["got unsupported message from ", show user]
   liftIO $ Logger.warning logh logmsg
   sendFailMsg h (uId user)
 processMessage h (TextMessage user mId txt t') =
@@ -301,12 +304,12 @@ processMessage h (TextMessage user mId txt t') =
 processEdit :: Bot.Handle -> Message -> IO ()
 processEdit h (UnsupportedMessage user) = do
   let logh = Bot.hLogger h
-  let logmsg = mconcat ["edit to unsupported message ", show user]
+      logmsg = mconcat ["edit to unsupported message ", show user]
   liftIO $ Logger.warning logh logmsg
 processEdit h m@(TextMessage user mId txt _) = do
   let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
-  let usrId = uId user
+      logh = Bot.hLogger h
+      usrId = uId user
   case reads (T.unpack txt) of
     [] -> liftIO $ Logger.warning logh $ mconcat ["cant parse edit ", show m]
     (amount, _) : _ -> do
@@ -316,16 +319,16 @@ processEdit h m@(TextMessage user mId txt _) = do
 processCallback :: MonadIO m => Bot.Handle -> CallbackQuery -> m ()
 processCallback h (CallbackQuery (User usrId) cid' "todayStats") = do
   let dbh = Bot.hDatabase h
-  let filename = T.unpack (cid' <> ".png")
-  let ph = Bot.hPlotter h
+      filename = T.unpack (cid' <> ".png")
+      ph = Bot.hPlotter h
   stats <- liftIO $ Database.getTodaysRecordsStats dbh usrId
   liftIO $ Plotter.plotDayStats ph stats filename
   liftIO $ editLastMessagePhoto h usrId filename
   liftIO $ removeIfExists filename
 processCallback h (CallbackQuery (User usrId) cid' "thisMonthStats") = do
   let dbh = Bot.hDatabase h
-  let filename = T.unpack (cid' <> ".png")
-  let ph = Bot.hPlotter h
+      filename = T.unpack (cid' <> ".png")
+      ph = Bot.hPlotter h
   stats <- liftIO $ Database.getMonthRecordsStats dbh usrId
   liftIO $ Plotter.plotMonthStats ph stats filename
   liftIO $ editLastMessagePhoto h usrId filename
@@ -343,14 +346,15 @@ answerCallback h cid text = sendRequest h "/answerCallbackQuery" query
     query = [("callback_query_id", Just cid), ("text", Just text)]
 
 processUpdate :: Bot.Handle -> Update -> StateT Tg IO ()
-processUpdate h (UpdateWithMessage _ msg) = processMessage h msg
-processUpdate h (UpdateWithEdit _ msg) = liftIO $ processEdit h msg
-processUpdate h (UpdateWithCallback _ cb) = processCallback h cb
+processUpdate h = \case
+  (UpdateWithMessage _ msg) -> processMessage h msg
+  (UpdateWithEdit _ msg) -> liftIO $ processEdit h msg
+  (UpdateWithCallback _ cb) -> processCallback h cb
 
 addRecordToDb :: MonadIO f => Bot.Handle -> Int -> Int -> T.Text -> UTCTime -> f ()
 addRecordToDb h usrId mId txt t = do
   let logh = Bot.hLogger h
-  let logmsg = mconcat ["cant parse new record from ", show usrId, ", msg text: ", T.unpack txt]
+      logmsg = mconcat ["cant parse new record from ", show usrId, ", msg text: ", T.unpack txt]
   case reads (T.unpack txt) of
     [] -> liftIO $ Logger.warning logh $ mconcat []
     (amount, _) : _ -> do
@@ -360,7 +364,7 @@ addRecordToDb h usrId mId txt t = do
 addUserToDb :: MonadIO m => Bot.Handle -> Int -> m ()
 addUserToDb h usrId = do
   let initkb = Bot.cInitKeyboard . Bot.hConfig $ h
-  let dbh = Bot.hDatabase h
+      dbh = Bot.hDatabase h
   liftIO $ Database.addUser dbh usrId
   liftIO $ mapM_ (Database.addButton dbh usrId) initkb
 
@@ -371,8 +375,8 @@ addButton h usrId (amount' : _) = do
     [] -> Logger.error (Bot.hLogger h) $ "bad parameter to /add command " ++ T.unpack amount'
     (amount, _) : _ -> do
       let dbh = Bot.hDatabase h
-      let logh = Bot.hLogger h
-      let logText = mconcat ["added button ", show amount, " for user ", show usrId]
+          logh = Bot.hLogger h
+          logText = mconcat ["added button ", show amount, " for user ", show usrId]
       Logger.info logh logText
       Database.addButton dbh usrId amount
 
@@ -383,8 +387,8 @@ removeButton h usrId (amount' : _) = do
     [] -> Logger.error (Bot.hLogger h) $ "bad parameter to /add command" ++ T.unpack amount'
     (amount, _) : _ -> do
       let dbh = Bot.hDatabase h
-      let logh = Bot.hLogger h
-      let logText = mconcat ["added button ", show amount, " for user ", show usrId]
+          logh = Bot.hLogger h
+          logText = mconcat ["added button ", show amount, " for user ", show usrId]
       Logger.info logh logText
       Database.removeButton dbh usrId amount
 
@@ -406,7 +410,7 @@ getKeyboard h usrId = do
 getTextWithAmount :: MonadIO m => Bot.Handle -> Int -> m (Maybe T.Text)
 getTextWithAmount h usrId = do
   let dbh = Bot.hDatabase h
-  let logh = Bot.hLogger h
+      logh = Bot.hLogger h
   sumOfaDay <- liftIO $ Database.getSumTodaysAmount dbh usrId
   case sumOfaDay of
     (Just (Just s)) -> do
